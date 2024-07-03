b0e51cd426b2910d4ef194f4c3bf1afe7bdd8b91
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-07-03 06:25:21 -0500

    update file conceptos basicos iniciales
    Add lessons -> conceptos iniciales

--- Python/Udemy Python 3 empieza desde cero/Conceptos basicos iniciales.py
+++ Python/Udemy Python 3 empieza desde cero/Conceptos basicos iniciales.py
@@ -56,3 +56,83 @@ print(f'tengo {10+10} años')
 
 print(cadena_1*5)
 
+#Vectores
+
+vector_1 = ["Python", "Django", "React", "Vue"]
+
+print(vector_1[0])
+
+#cuando se usa numero negativos 
+#empieza por el final
+print(vector_1[-1])
+
+#modificar en el vector
+vector_1[1] = "Flutter"
+
+#añadir un dato al final del vector
+vector_1.append("Angular")
+print(vector_1[4])
+
+#añadir un dato en posicion especifica del vector
+vector_1.insert(1,"Angular")
+print(vector_1[1])
+
+#Tuplas
+#diferencia con vector, 
+#sus datos no se pueden modificar
+
+#en vectores se usa [] en tuplas()
+tupla_1 = ("python", "Django", "React", "Vue")
+print(tupla_1[1])
+
+#Diccionarios
+#diccionario son datos relacionados 
+data ={"name":"Ceraspio,", "last_name": "Joso", "edad": "18"}
+
+#nos permite imprimir datos llamando al nombre
+#indice o nombre de variable
+#imprimir un dato del diccionario
+print(data["last_name"])#imprime "Joso"
+
+#imprimir con concatenacion
+print(f"mi apellido es {data["last_name"]}")#imprime "Joso"
+
+#añadir datos al diccionario
+data["city"] = "Veraguas"
+print(data["city"])
+
+#eliminar un dato de diccionario
+del data["edad"]#elimina la clave "edad" del diccionario
+
+#saber cuantas claves hay en el diccionario
+print(len(data))
+
+#recibir datos por consola
+name = input("introduce tu nombre: ")
+last_name = input("introduce tu apellido: ")
+edad = input("introduce tu edad: ")
+
+print(f"Me llamo {name} {last_name} y tengo {edad} años")
+
+#operar numeros ingresados por consola
+num_1 = input("primer numero: ")
+num_2 = input("segundo numero: ")
+
+#para poder operar los numeros introducidos 
+#se tiene que modificar el tipo de variable 
+#ya que la funcion input los almacena como string
+print(int(num_1)+int(num_2))
+
+#control de datos 
+#se puede cambiar el tipo de dato desde el input
+num_1 =int(input("primer numero: "))
+num_2 = int(input("segundo numero: "))
+
+print(num_1 + num_2)
+
+#ejemplo tipos de datos a los que se puede cambiar
+#float, int, bool, str
+
+#se modifica de int a bool
+num_1 =int(input("primer numero: "))
+print(bool(num_1)) #imprimira True si es >= 1
