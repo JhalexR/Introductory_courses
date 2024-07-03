3a5a5a244edfc602766a1350fe300382a83b4d63
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-07-03 06:58:38 -0500

    Update Condicional anidado elif.py
    Comments hierarchy and use of if - else and if-elseif-else

--- Python/Udemy Python 3 empieza desde cero/Condicional anidado elif.py
+++ Python/Udemy Python 3 empieza desde cero/Condicional anidado elif.py
@@ -1,12 +1,22 @@
-edad = int(input("introduce tu edad: "))
-mes = int(input("introduce tu mes de nacimiento: "))
+# la sentencia If-Elif-Else
+
+# Las sentencias IF e If-else solo son útiles para situaciones binarias.
+
+# En el caso de un problema condicional múltiple, se utiliza la sentencia 
+# «if-elif-else».
+
+# En primer lugar, se comprueba la condición de la sentencia If. 
+# Si es falsa, se evalúa la sentencia Elif. 
+# Si la condición también es falsa, se evalúa la sentencia Else.
 
 # En Python, la sentencia «Elif» se utiliza para comprobar múltiples
 # condiciones si una condición es falsa. 
 # Es similar a la sentencia «If-Else», 
 # pero la diferencia es que «Elif» 
-# evalúa múltiples condiciones a diferencia de «Else».    
-
+# evalúa múltiples condiciones a diferencia de «Else».  
+  
+edad = int(input("introduce tu edad: "))
+mes = int(input("introduce tu mes de nacimiento: "))
 
 if edad >= 18:
     print("Eres mayor de edad")
