95ab950905ee7773c41f1a762561494b989c854c
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-07-05 07:30:58 -0500

    Create Ciclo for.py
    lesson for in python

---
+++ Python/Udemy Python 3 empieza desde cero/Ciclo for.py
@@ -0,0 +1,8 @@
+numeros = [1, 55, 88, 43, 65, 90, 100]
+
+# en el ciclo for se declara una variable que almacena el contenido del 
+# vector en ella mientras va avanzando automaticamente en las posiciones
+# sin necesidad de declarar el incremento +1 
+
+for num in numeros:
+    print(num)
