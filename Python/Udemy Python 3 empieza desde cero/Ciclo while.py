fd215500160b3b532ce51ca03e06260177c0f8c6
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-07-07 10:32:44 -0500

    Create Ciclo while.py
    file about while

---
+++ Python/Udemy Python 3 empieza desde cero/Ciclo while.py
@@ -0,0 +1,12 @@
+count = 1
+
+while count <= 50:
+    print(count)
+    count = count+1
+    
+count = 1
+tabla = int(input('Que tabla quieres ver? '))
+
+while count <= 10:
+    print(f'{count} x {tabla} = {tabla*count}')
+    count = count+1
