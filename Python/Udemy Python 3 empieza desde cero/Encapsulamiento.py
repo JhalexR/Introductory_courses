3478ac9cc03b7065f0a5efa6fbbafa0a46ad1ee6
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-07-07 14:36:58 -0500

    Update Encapsulamiento.py
    Add comments

--- Python/Udemy Python 3 empieza desde cero/Encapsulamiento.py
+++ Python/Udemy Python 3 empieza desde cero/Encapsulamiento.py
@@ -2,7 +2,7 @@
 
 # Encapsulamiento en Python
 # En Python, el encapsulamiento se logra mediante convenciones de nombres
-#  y la implementación de métodos. 
+# y la implementación de métodos. 
 # Existen tres niveles de acceso a los atributos y métodos de una clase:
 
 # Público (Public): Atributos y métodos que están disponibles desde fuera de la clase. 
@@ -35,24 +35,48 @@ class Persona:
     def acceso_salario(self):
         return self.__mostrar_salario()  # Método público que accede a un método privado
 
-
 # Crear una instancia de la clase Persona
 persona = Persona("Juan", 30)
 
-# Acceso a un atributo público
+# Atributos Públicos: nombre es un atributo público y se puede
+# acceder directamente desde fuera de la clase.
 print(persona.nombre)  # Salida: Juan
 
-# Acceso a un atributo protegido (se puede, pero no se recomienda)
+# Atributos Protegidos: _edad es un atributo protegido. 
+# Por convención, no debería ser accedido directamente desde fuera de la clase, 
+# aunque Python lo permite. (se puede, pero no se recomienda)
 print(persona._edad)  # Salida: 30
 
-# Intentar acceder a un atributo privado (esto generará un error)
+# Atributos Privados: __salario es un atributo privado. 
+# No se puede acceder directamente desde fuera de la clase.
+# al intentar acceder a un atributo privado esto generará un error
 # print(persona.__salario)  # AttributeError
 
+# acceso_salario es un método público que llama 
+# al método privado __mostrar_salario.
 # Acceder al atributo privado mediante un método público
 print(persona.acceso_salario())  # Salida: 50000
 
 # Acceso a un método protegido (se puede, pero no se recomienda)
 print(persona._mostrar_edad())  # Salida: 30
 
+# __mostrar_salario es un método privado, que no se puede llamar
+# directamente desde fuera de la clase.
 # Intentar acceder a un método privado (esto generará un error)
 # print(persona.__mostrar_salario())  # AttributeError
+
+# Name Mangling
+# Python aplica una técnica llamada name mangling para atributos y métodos privados, 
+# lo que renombra internamente estos atributos y métodos para evitar colisiones. 
+# Por ejemplo, __salario se renombra internamente como _Persona__salario. 
+# Aunque se puede acceder a estos atributos y métodos renombrados, 
+# no es una práctica recomendada.
+
+# Acceso a un atributo privado utilizando name mangling
+print(persona._Persona__salario)  # Salida: 50000
+
+#El encapsulamiento en Python es una forma eficaz de proteger 
+# los datos y asegurarse de que solo se acceda a ellos de manera controlada,
+# a través de métodos definidos en la clase. 
+# Esto ayuda a mantener la integridad de los datos y a seguir el principio 
+# de ocultación de información en la programación orientada a objetos.
