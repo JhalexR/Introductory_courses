79653ea9a88604c8dcbf57596caacc5feea18096
John A Penaloza R <rojas.jhalex@gmail.com>
2024-07-09 09:30:32 -0500

    Create Curso Excel Python.py
    first lesson course

---
+++ Python/Evoluciona desde Excel a Python/Curso Excel Python.py
@@ -0,0 +1,11 @@
+# Curso Excel Python
+
+# importando librerias importante instalarlas anteriormente
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Cargando base de datos
+
+# 1 metodo de carga base de datos desde archivo .CSV
+fifa = pd.read_csv("")
