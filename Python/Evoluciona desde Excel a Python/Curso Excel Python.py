2b849ecb6187aec1d13d082b8ba84aed6fc854d2
John A Penaloza R <rojas.jhalex@gmail.com>
2024-07-10 14:35:52 -0500

    Update Curso Excel Python.py
    Lesson -> Filters multiples options

--- Python/Evoluciona desde Excel a Python/Curso Excel Python.py
+++ Python/Evoluciona desde Excel a Python/Curso Excel Python.py
@@ -29,20 +29,62 @@ fifa_2 = pd.read_excel(r"C:\Users\JohnAlexanderPeñaloz\OneDrive - PayU\Principa
 # revision de la base de datos
 
 # funcion head muestra las 5 primeras filas de la base de datos con todas las columnas
-print(fifa.head())
+fifa.head()
 
 # funcion muestra el nombre de las columnas de la base de datos
-print(fifa.columns.values)
+fifa.columns.values
 
 # funcion muestra el nombre de las columnas de la base de datos en en una fila
-print(fifa.columns.values.tolist)
+fifa.columns.values.tolist
 
 # funcion muestra la cantidad de filas y columnas de la base de datos en en una fila
-print(fifa.shape)
+fifa.shape
 
 # funcion muestra las principales medidas estadisitcas de las columnas numericas
-print(fifa.describe())
+fifa.describe()
 
 # funcion muestra informacion importante de la base como datos nullos y tipo de datos
-print(fifa.info())
+fifa.info()
 
+# filtrar bases de datos
+
+# filtro por nombre de columnas
+fifa_filtro_1 = fifa[["ID","Name","Age"]]
+
+# filtro por funcion drop - eliminar columnas o filas
+fifa_elim_column = fifa.drop("Unnamed: 0", axis=1) # eliminar comlumna axis = 1, eliminar fila axis = 0
+fifa_elim_columns = fifa.drop(["Unnamed: 0", "ID"], axis=1) # eliminar varias comlumnas [ ]
+# para eliminar filas se coloca el numero de la fila
+fifa_elim_fila = fifa.drop(0, axis=0) # elimina la fila en la posicion 0
+
+# filtro por loc e iloc
+
+# filtro por loc
+fifa_loc = fifa.loc[:,["Name", "Age"]]# para filtrar por loc es por nombre de la columna
+
+# filtro por iloc
+fifa_iloc = fifa.iloc[0:10] # filtra las primeras 10 filas
+fifa_iloc = fifa.iloc[10] # filtra solo la fila 10
+fifa_iloc = fifa.iloc[0:10,0:10] # filtra las primeras 10 filas y las primeras 10 columnas
+fifa_iloc = fifa.iloc[:,0:10] # para filtra solo las primeras 10 columnas pero no filtrar ninguna fila
+fifa_iloc = fifa.iloc[0:10,:] # para filtra solo las primeras 10 filas pero no filtrar ninguna columna
+fifa_iloc = fifa.iloc[:,[2,5,9]] # para filtra por columnas
+
+# Filtrando base de dtos con la que se va a trabajar con iloc
+fifa_final = fifa.iloc[:,1:28] # se filtran las primeras 28 columnas que son las que se van a usar
+
+# filtro por valores especificos - similar a Excel -
+Fifa_barcelona = fifa[fifa["Club"]=="FC Barcelona"] # filtrando por el valor Club Barcelona 
+Fifa_dif_barcelona = fifa[fifa["Club"]!="FC Barcelona"] # filtrando por el valor diferentes al Club Barcelona
+Fifa_menor_30 = fifa[fifa["Age"]<30] # filtrando por jugadores menores de 30 años
+
+# filtro compuesto mas de dos categorias
+fifa_mas_30_Brasil = fifa[(fifa["Age"]>30)&(fifa)["Nationality"]=="Brazil"] # filtro de jugadores mayores a 30 años y de Brasil
+fifa_mas_30_Brasil = fifa[(fifa["Age"]>30)|(fifa)["Nationality"]=="Brazil"] # filtro de jugadores mayores a 30 años o de Brasil
+
+#filtro por valores de texto que contengan parte de una palabra 
+fifa_parte_nombre = fifa[fifa["Name"].str.contains("Sergio")] # filtrar elementos que en la columna "Name" contengan la parte "Sergio"
+
+# filtro funcion .isin filtra por los criterios de una lista
+lista = ["Brazil", "Chile", "Argentina"]
+fifa_lista = fifa[fifa["Nationality"].isin(lista)]
