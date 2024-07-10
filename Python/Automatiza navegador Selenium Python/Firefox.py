4c2278f7a6a1757e6f88a648816f1663d61b00dd
Alexander Peñaloza <lgc90smart@gmail.com>
2024-07-10 07:11:00 -0500

    fixed errors
    file -> Mapa primeras lecciones -> error concepto
    
    file -> Firefox -> sintaxis erronea

--- Python/Automatiza navegador Selenium Python/Firefox.py
+++ Python/Automatiza navegador Selenium Python/Firefox.py
@@ -1,4 +1,14 @@
 from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get("http://clouditeducation.com/pruebas/")
+
+# importar by para buscar elementos en la pagina con selenium
+from selenium.webdriver.common.by import By
+elemento = driver.find_element(By.ID,"noImportante")
+if elemento is not None:
+    print("El elemento fue encontrado")
+
+# importar time para usr tiempos en el programa
+import time
+time.sleep(5)
 driver.quit()
