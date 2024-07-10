4be5bb35661fee6e853ac69c3708e7ff5a77b00d
Alexander Peñaloza <lgc90smart@gmail.com>
2024-07-10 07:35:17 -0500

    Update Firefox.py
    lesson -> find by name

--- Python/Automatiza navegador Selenium Python/Firefox.py
+++ Python/Automatiza navegador Selenium Python/Firefox.py
@@ -4,7 +4,14 @@ driver.get("http://clouditeducation.com/pruebas/")
 
 # importar by para buscar elementos en la pagina con selenium
 from selenium.webdriver.common.by import By
+
+# buscar un elemento por ID
 elemento = driver.find_element(By.ID,"noImportante")
+if elemento is not None:
+    print("El elemento fue encontrado")
+
+# buscar un elemento por NAME
+elemento = driver.find_element(By.NAME,"ultimo")
 if elemento is not None:
     print("El elemento fue encontrado")
 
