078a9f1ea8b1341a45c7303eacb50fffa2294ea5
Alexander Peñaloza <lgc90smart@gmail.com>
2024-07-12 07:55:50 -0500

    Update Firefox.py
    Search element by XPath and XPath copy process

--- Python/Automatiza navegador Selenium Python/Firefox.py
+++ Python/Automatiza navegador Selenium Python/Firefox.py
@@ -15,7 +15,19 @@ elemento = driver.find_element(By.NAME,"ultimo")
 if elemento is not None:
     print("El elemento fue encontrado")
 
-# importar time para usr tiempos en el programa
+# buscar elemento por Xpath
+elemento = driver.find_element(By.XPATH,"//[@id='noImportante']/td[1]") 
+if elemento is not None:
+    print("El elemento fue encontrado")
+# para saber el xpath debemos buscarlo en  el navegador:
+# F12
+# Herramienta de seleccion
+# seleccionamos la parte que queremos saber el Xpath
+# el panel o consola de desarrollador muestra su ubicacion en el HTML
+# en el HTML le damos click derecho a la parte que queremos saber xpath
+# le damos la opcion copiar -> xpath
+
+# importar time para usar tiempos en el programa
 import time
 time.sleep(5)
 driver.quit()
