7a74e65fbbb96d2b387f3335ed4a55786c972a35
Alexander Peñaloza <lgc90smart@gmail.com>
2024-07-26 09:17:06 -0500

    Update lesson inheritance
    Save files inheritance

--- Java/Udemy_Java_Basico/Orientada_a_objetos/Herencia/Adicion.java
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Herencia/Adicion.java
@@ -1,6 +1,9 @@
 package Udemy_Java_Basico.Orientada_a_objetos.Herencia;	
 
-public class Adicion extends Aritmetica{
+// Extends indica la herencia entre una clase secundaria y una principal. 
+// Extends En Java es una palabra clave que se escribe con la clase secundaria 
+// durante la declaración de la clase seguida del nombre de la clase principal.
+public class Adicion extends Aritmetica{ // clase secundaria de la clase principal Aritmetica
 	public void resultado(){
 		deducir=dato1+dato2;
 
