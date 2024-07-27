cfd8882241478ba32025ba395c7756431a74739f
Alexander Peñaloza <lgc90smart@gmail.com>
2024-07-27 09:56:25 -0500

    Lesson on Polymorphism Method Overload
    Create folder -> Polimorfismo_sobrecarga_metodos
    Create file -> Class, Lectura
    Create file -> Class Demo

---
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobrecarga_metodos/Demo.java
@@ -0,0 +1,12 @@
+package Udemy_Java_Basico.Orientada_a_objetos.Polimorfismo_sobrecarga_metodos;
+
+public  class Demo {
+     public Static void main(String[] args) { 
+           Lectura objeto = new Lectura();
+           // Se invoca el mismo metodo y se usa el que coincida con el tipo 
+           // y cantidad de argumentos
+           objeto.print(10);   
+           objeto.print(“arte”);
+           objeto.print(“hola”, 34);
+      }
+}   
---
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobrecarga_metodos/Lectura.java
@@ -0,0 +1,16 @@
+package Udemy_Java_Basico.Orientada_a_objetos.Polimorfismo_sobrecarga_metodos;
+
+// el polimorfismo se puede realizar por sobre carga de métodos como en este jemplo
+class  Lectura {
+   // Los metodos se llaman igual se invocan desde la otra clase dependiendo
+   // de la cantidad y pipo de datos de los argumentos
+   public void print (String s){
+       System.out.println(“primer método con sólo un string- “ + s);
+    }
+    public void print (int i){
+       System.out.println(“primer método con sólo un entero- “ + i);
+    }
+     public void print (String s, int i){
+       System.out.println(“primer método con un string y un entero “ + s);
+    }
+}
