d3e55eb6361aad28a970aed456ec24eb07ec51e8
Alexander Peñaloza <lgc90smart@gmail.com>
2024-07-27 10:24:57 -0500

    Fixed bugs
    Name in classes inherited from the Abstract Class corrected

--- Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobre_escritura_metodos/Ejecucion.java
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobre_escritura_metodos/Ejecucion.java
@@ -2,9 +2,9 @@ package Udemy_Java_Basico.Orientada_a_objetos.Polimorfismo_sobre_escritura_metod
 
 public class Ejecucion {
 	public static void main(String[] args) {
-		Animal2 objeto = new Perro2();
-		Animal2 objeto2 = new Gato();
-		Animal2 objeto3 = new Caballo2();
+		Perro2 objeto = new Perro2();
+		Gato objeto2 = new Gato();
+		Caballo2 objeto3 = new Caballo2();
 		objeto.sonido();
 		objeto2.sonido();
 		objeto3.sonido();
--- Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobre_escritura_metodos/Gato.java
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobre_escritura_metodos/Gato.java
@@ -1,6 +1,6 @@
 package Udemy_Java_Basico.Orientada_a_objetos.Polimorfismo_sobre_escritura_metodos;
 
-public class Gato extends Animal() {
+public class Gato extends Animal2() {
 	public void sonido() {
 		System.out.println("el sonido es maullar");
 	}
