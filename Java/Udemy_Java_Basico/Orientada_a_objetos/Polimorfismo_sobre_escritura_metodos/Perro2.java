6c38bf245e484adf9bdaff4d85353c68fc79d0af
Alexander Peñaloza <lgc90smart@gmail.com>
2024-07-27 10:24:57 -0500

    Fixed bugs
    Name in classes inherited from the Abstract Class corrected

--- Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobre_escritura_metodos/Perro2.java
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobre_escritura_metodos/Perro2.java
@@ -1,6 +1,6 @@
 package Udemy_Java_Basico.Orientada_a_objetos.Polimorfismo_sobre_escritura_metodos;
 
-public class Perro2 extends Animal {
+public class Perro2 extends Animal2 {
 	public void sonido() {
 		System.out.println("el sonido es ladrar");
 	}
