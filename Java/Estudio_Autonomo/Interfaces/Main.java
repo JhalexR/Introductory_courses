3e859ce1bf7aca35b48d50446e7f3d275a4d011d
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-12 15:08:28 -0500

    Update Main.java
    Add Class Main & method main

--- Java/Estudio_Autonomo/Interfaces/Main.java
+++ Java/Estudio_Autonomo/Interfaces/Main.java
@@ -1,5 +1,13 @@
-package Estudio_Autonomo;
-
+package Estudio_Autonomo.Interfaces;
+// Clase principal para ejecutar el código
 public class Main {
-
+    public static void main(String[] args) {
+        // Crear una instancia (objeto) de Perro
+        Perro miPerro = new Perro("Rex");
+        
+        // Llamar a métodos del objeto
+        miPerro.saludar();
+        miPerro.hacerSonido();
+    }
 }
+
