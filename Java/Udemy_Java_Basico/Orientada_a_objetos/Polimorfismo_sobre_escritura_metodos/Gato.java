23dd54812b8027b32b3dad06e24afe9ba4f5ddce
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-12 14:53:16 -0500

    Update Gato.java
    Fixed sintaxis error

--- Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobre_escritura_metodos/Gato.java
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobre_escritura_metodos/Gato.java
@@ -1,7 +1,7 @@
 package Udemy_Java_Basico.Orientada_a_objetos.Polimorfismo_sobre_escritura_metodos;
 
-public class Gato extends Animal2() {
+public class Gato extends Animal2 {
 	public void sonido() {
 		System.out.println("el sonido es maullar");
-	}	
+	}
 }
