424c8bd61540dd33156fc2a9fa0a418802d25d0e
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-14 14:36:34 -0500

    Create Rectangulo.java
    Lesson on interfaces

---
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Interfaces_2/Rectangulo.java
@@ -0,0 +1,7 @@
+package Udemy_Java_Basico.Orientada_a_objetos.Interfaces_2;
+
+class Rectangulo implements Dibujar{  
+    public void draw(){
+      System.out.println("drawing rectangle");
+    }  
+  }  
