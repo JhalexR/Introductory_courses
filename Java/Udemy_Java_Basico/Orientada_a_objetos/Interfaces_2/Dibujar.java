5b4c3c4d719467a505d427f64d394d89dcaa1429
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-14 14:44:55 -0500

    Update Dibujar.java
    add coments file contains interface

--- Java/Udemy_Java_Basico/Orientada_a_objetos/Interfaces_2/Dibujar.java
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Interfaces_2/Dibujar.java
@@ -1,5 +1,9 @@
 package Udemy_Java_Basico.Orientada_a_objetos.Interfaces_2;	
 
+// La interfaz define el comportamiento de una clase
+// si esta interfaz es implementada por una clase 
+// tod_os los metodos de la clase deben realizarse
+// la palabra reservada “interface”  para definir la interfaz
 public interface Dibujar {
     void draw();  
 }
