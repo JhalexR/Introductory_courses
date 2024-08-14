09f3f2168bf8b5edea3087fbe2d5a095707c2f2f
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-14 14:37:52 -0500

    Create Circulo2.java
    Lesson on Interfaces

---
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Interfaces_2/Circulo2.java
@@ -0,0 +1,7 @@
+package Udemy_Java_Basico.Orientada_a_objetos.Interfaces_2;
+
+class Circulo2 implements Dibujar{  
+    public void draw(){
+        System.out.println("drawing circle");
+    }  
+  }  
