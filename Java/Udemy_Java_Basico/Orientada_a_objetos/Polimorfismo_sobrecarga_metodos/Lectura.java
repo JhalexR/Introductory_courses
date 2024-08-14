21f12eefcc75a19354e420d480a1400965f4b455
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-14 10:48:53 -0500

    Update Lectura.java
    fixed comments

--- Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobrecarga_metodos/Lectura.java
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Polimorfismo_sobrecarga_metodos/Lectura.java
@@ -3,7 +3,7 @@ package Udemy_Java_Basico.Orientada_a_objetos.Polimorfismo_sobrecarga_metodos;
 // el polimorfismo se puede realizar por sobre carga de métodos como en este jemplo
 public class Lectura {
    // Los metodos se llaman igual se invocan desde la otra clase dependiendo
-   // de la cantidad y pipo de datos de los argumentos
+   // de la cantidad y tipo de datos de los argumentos
    public void print (String s){
        System.out.println("primer método con sólo un string- " + s);
     }
