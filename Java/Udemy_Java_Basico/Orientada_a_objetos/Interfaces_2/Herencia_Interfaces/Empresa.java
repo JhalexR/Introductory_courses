f258dc1485f019e1d523562efd8ead5b19eb9dde
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-14 16:12:01 -0500

    Create Empresa.java
    lesson on interface inheritance, file contains the extended interface inherits from the main interface

---
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Interfaces_2/Herencia_Interfaces/Empresa.java
@@ -0,0 +1,7 @@
+package Udemy_Java_Basico.Orientada_a_objetos.Interfaces_2.Herencia_Interfaces;
+
+// Interfaz que hereda desde la interfaz padre "Bank" 
+
+interface Empresa extends Bank {
+  public void datos();
+}
