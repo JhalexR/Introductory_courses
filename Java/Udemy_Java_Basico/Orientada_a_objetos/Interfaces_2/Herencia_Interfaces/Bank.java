e6245a7203f8199013a4d1396edfdc6a45df4589
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-14 16:11:06 -0500

    Create Bank.java
    lesson on interface inheritance, file contains the main interface from which other interfaces inherit

---
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Interfaces_2/Herencia_Interfaces/Bank.java
@@ -0,0 +1,9 @@
+package Udemy_Java_Basico.Orientada_a_objetos.Interfaces_2.Herencia_Interfaces;
+
+// Interfaz principal o padre de la cual se pueden heredar mas interfaces
+// contiene dos metodos y se deben usar en la clase que implemente la interfaz 
+
+interface Bank{  
+  float rateOfInterest(); 
+  public void visualizar(); 
+} 
