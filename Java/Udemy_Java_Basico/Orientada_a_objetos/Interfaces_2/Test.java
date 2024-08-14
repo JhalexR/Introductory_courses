ed32a060e643b4ca8c60f4e5c79e1d8abe7e33ac
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-14 14:39:26 -0500

    Create Test.java
    Lesson on Interface

---
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Interfaces_2/Test.java
@@ -0,0 +1,10 @@
+package Udemy_Java_Basico.Orientada_a_objetos.Interfaces_2;
+
+public class Test {
+    public static void main(String args[]){  
+        Circulo2 d=new Circulo2(); 
+        Rectangulo c=new Rectangulo();
+        d.draw();  
+        c.draw();
+      }
+}
