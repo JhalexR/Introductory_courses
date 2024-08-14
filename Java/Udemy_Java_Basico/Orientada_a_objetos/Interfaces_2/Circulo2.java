5b3da33dc8e51e2367e564da7926eb4e0b9ee15c
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-14 15:18:35 -0500

    Update Circulo2.java
    Add comments class implements interface

--- Java/Udemy_Java_Basico/Orientada_a_objetos/Interfaces_2/Circulo2.java
+++ Java/Udemy_Java_Basico/Orientada_a_objetos/Interfaces_2/Circulo2.java
@@ -1,5 +1,11 @@
 package Udemy_Java_Basico.Orientada_a_objetos.Interfaces_2;
 
+// en esta clase se implementa la interfaz 
+// en la definicion de la clase se debe usar 
+// la palabra reservada "implements" para usar
+// la interfaz en la clase y usar los metodos de
+// la interfaz
+
 class Circulo2 implements Dibujar{  
     public void draw(){
         System.out.println("drawing circle");
