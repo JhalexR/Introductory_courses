0ac5ce65ce5809a449a9d622043c0d0f7c8608e9
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-19 10:39:53 -0500

    Update egStatic.java
    Update comments

--- Java/Udemy_Java_Basico/Palabras_reservadas/Static_sintaxis/egStatic.java
+++ Java/Udemy_Java_Basico/Palabras_reservadas/Static_sintaxis/egStatic.java
@@ -10,9 +10,9 @@ public class egStatic {
 public static void main(String[] args) {
   egStatic.fun();  // se accede directamente con la clase al metodo "fun" sin necesidad de crear objeto
   System.out.println(egStatic.x); // se accede directamente al valor de "x" sin necesidad de crear objeto
-  egStatic S1 = new egStatic(); // creacion de primer objeto
-  egStatic S2 = new egStatic(); // creacion de segundo objeto
-  System.out.println(S1.x); // uso de primer objeto se puede acceder a la variable "static" con un objeto pero no es necesario
-  S2.fun(); // uso de segundo objeto se puede acceder al metodod "static" con un objeto pero no es necesario
+  // egStatic S1 = new egStatic(); // creacion de primer objeto, se coloca como comentario debido a que genera un "warning" recomendando usar clase
+  // egStatic S2 = new egStatic(); // creacion de segundo objeto, se coloca como comentario debido a que genera un "warning" recomendando usar clase
+  //System.out.println(S1.x); // uso de primer objeto se puede acceder a la variable "static" con un objeto pero no es necesario se coloca como comentario debido a que genera un "warning" recomendando usar clase
+  // S2.fun(); // uso de segundo objeto se puede acceder al metodod "static" con un objeto pero no es necesario, se coloca como comentario debido a que genera un "warning" recomendando usar clase
 }
 }
