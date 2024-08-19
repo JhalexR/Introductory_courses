de45c24c385c34c1cb2a0f21e5d8ba1d8771f31b
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-19 07:53:13 -0500

    Create MiExcepcionPersonalizada.java
    Lesson on try catch, custom exception, class extends

---
+++ Java/Udemy_Java_Basico/Bloque_Try_Catch/Exepcion_personalizada/MiExcepcionPersonalizada.java
@@ -0,0 +1,8 @@
+package Udemy_Java_Basico.Bloque_Try_Catch.Exepcion_personalizada;
+
+public class MiExcepcionPersonalizada extends Exception {
+    public MiExcepcionPersonalizada(String mensaje) {
+        super(mensaje);
+    }
+}
+
