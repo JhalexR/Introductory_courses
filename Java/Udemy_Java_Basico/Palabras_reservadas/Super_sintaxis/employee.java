a63ee33185a69a42e1ad51a30fe415138309c2d7
Alexander Peñaloza <lgc90smart@gmail.com>
2024-08-19 11:55:45 -0500

    Create employee.java
    Lesson on sintaxis "super" parent class and child class attributes with the same name

---
+++ Java/Udemy_Java_Basico/Palabras_reservadas/Super_sintaxis/employee.java
@@ -0,0 +1,23 @@
+package Udemy_Java_Basico.Palabras_reservadas.Super_sintaxis;
+
+class employee { // clase principal
+ int wt = 8; // atributo de la clase principal 
+}
+
+class clerk extends employee { // 
+ int wt = 10;  // atributo de la clase secundaria tiene el mismo nombre del atributo la clase principal
+ void display() {
+  System.out.println(super.wt); // aca la expresion "super.wt" hace referencia al atributo "wt" de la clase "employee" y no al de clase "clerk", se usa "super" porque tienen el mismo nombre
+ }
+
+ public static void main(String args[]) {
+  clerk c = new clerk();
+  c.display();
+ }
+
+}
+/*Instance refiere una variable de instancia de la clase actual de forma 
+predeterminada, pero cuando debe referir la variable de instancia de clase
+ principal, debe usar la palabra clave super para distinguir entre la variable 
+ de instancia de la clase principal (aquí empleado) y la variable de instancia de 
+ la clase actual (aquí, empleado) .*/
