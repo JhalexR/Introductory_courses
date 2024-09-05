ae924b9c6b7f81a99408fdb487de9c99bdcb308a
Alexander Peñaloza <lgc90smart@gmail.com>
2024-09-05 18:29:22 -0500

    Update String_of_char.java
    The method is created to display the longest word in an array, it is completed in the main method

--- Java/Estructura_de_Datos/Subgrupo_29/Primera_entrega_Ejercicio_3/String_of_char.java
+++ Java/Estructura_de_Datos/Subgrupo_29/Primera_entrega_Ejercicio_3/String_of_char.java
@@ -1,5 +1,9 @@
 package Estructura_de_Datos.Subgrupo_29.Primera_entrega_Ejercicio_3;
 
+/*Author: John Alexander Peñaloza Rojas
+ * 
+*/
+
 import java.util.Scanner;
 
 public class String_of_char {
@@ -17,7 +21,7 @@ public class String_of_char {
         Dat = new Scanner(System.in); 
         array_size = Dat.nextInt();
         sequence = new String[array_size-1];
-        for(int f=0;f<array_size;f++) 
+        for(int f = 0;f < array_size; f++) 
             sequence[f] = null;
         chain_size = 0;
         prompter = 0;
@@ -25,17 +29,20 @@ public class String_of_char {
     }
 
 
-    public void fill_out_array (){
+    public void fill_out_array () {
 
-        System.out.println("Escribe una frase, al finalizar presiona enter: ");
-        for(int f=0;f<array_size;f++)        
+        System.out.println("Escribe una frase, y al finalizar presiona enter: ");
+        for(int f = 0;f < array_size; f++)        
             sequence[f] = Dat.nextLine();
+        
+        
 
     }
 
 
-    public void loop_array(int chain_size, int prompter, int f){
+    public void loop_array () {
         
+        int chain_size, prompter, f;
         chain_size = prompter = f = 0;
         do  {
             if (sequence[f] != " ") {
@@ -44,6 +51,7 @@ public class String_of_char {
             } else if (chain_size > this.chain_size){
                 this.chain_size = chain_size;
                 this.prompter = prompter;
+                chain_size = 0;
                 f++;
                 if (sequence[f] != " ")
                     prompter = f; 
@@ -54,10 +62,24 @@ public class String_of_char {
                 f++;            
         } while (sequence[f] != null);
 
+    }
+
+
+    public void longest_word () {
+
+        System.out.println("La palabra mas larga en el arreglo inicia en el indice: "+prompter);
+        System.out.println("La palabra mas larga en el arreglo es: ");
+        for(int f = prompter; f < chain_size; f++) 
+            System.out.print(sequence[f]);
 
     }
 
     public static void main(String[] args) {
+
+        String_of_char String_1 = new String_of_char();
+        String_1.fill_out_array();
+        String_1.loop_array();
+        String_1.longest_word();        
         
     }
     
