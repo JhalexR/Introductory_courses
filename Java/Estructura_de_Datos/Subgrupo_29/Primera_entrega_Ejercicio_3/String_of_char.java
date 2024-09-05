3d31a1a5d935ea4d1604ad64a730d0d7a959d8bb
Alexander Peñaloza <lgc90smart@gmail.com>
2024-09-05 17:00:19 -0500

    Update String_of_char.java
    attributes are created,
    The constructor method was created,
    the fill out arrangement method is created,
    the main method is created

--- Java/Estructura_de_Datos/Subgrupo_29/Primera_entrega_Ejercicio_3/String_of_char.java
+++ Java/Estructura_de_Datos/Subgrupo_29/Primera_entrega_Ejercicio_3/String_of_char.java
@@ -1,5 +1,39 @@
+package Estructura_de_Datos.Subgrupo_29.Primera_entrega_Ejercicio_3;
 
+import java.util.Scanner;
 
 public class String_of_char {
+
+    protected String sequence[];    
+    protected int array_size;
+    protected int chain_size;
+    protected int prompter;
+    protected Scanner Dat;
+
+    public String_of_char(){
+        
+        System.out.print("Introduce el tamaño del arreglo: ");
+        Dat = new Scanner(System.in); 
+        array_size = Dat.nextInt();
+        sequence = new String[array_size-1];
+        for(int f=0;f<array_size;f++) 
+            sequence[f] = null;
+        chain_size = 0;
+        prompter = 0;
+
+    }
+
+    public void fill_out_array (){
+
+        System.out.println("Escribe una frase, al finalizar presiona enter: ");
+        for(int f=0;f<array_size;f++)        
+            sequence[f] = Dat.nextLine();
+            
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
     
 }
