b705a96465cb40445c1644362ce31953795381ab
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-10-10 08:33:18 -0500

    Update HashTable2.java
    Errors Fixed

--- Java/Estructura_de_Datos/Tablas_Hash/HashTable2.java
+++ Java/Estructura_de_Datos/Tablas_Hash/HashTable2.java
@@ -1,11 +1,13 @@
-import java.util.HashTable; /* La declaracion de la tabla hash se realiza importando este conjunto */
+package Estructura_de_Datos.Tablas_Hash;
+
+import java.util.Hashtable; /* La declaracion de la tabla hash se realiza importando este conjunto */
 
 public class HashTable2 {
 
     public static void main(String[ ] args){
 
-        int aux = {49, 43, 65, 45, 67, 68, 97};
-        HashTable<Integer, Integer> tablahash = new HashTable<>();
+        int aux[] = {49, 43, 65, 45, 67, 68, 97};
+        Hashtable<Integer, Integer> tablahash = new Hashtable<>();
         /* La palabra reservada es HashTable <Tipo de dato, tipo de llave>
            seguido por el nombre de la tabla = la creacion de objeto */
 
