5a234b844ada574ef18fc1a625f9c5ae2014e616
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-10-10 08:52:33 -0500

    Update HashTable2.java
    the functions query, delete, empty are added

--- Java/Estructura_de_Datos/Tablas_Hash/HashTable2.java
+++ Java/Estructura_de_Datos/Tablas_Hash/HashTable2.java
@@ -4,17 +4,31 @@ import java.util.Hashtable; /* La declaracion de la tabla hash se realiza import
 
 public class HashTable2 {
 
-    public static void main(String[ ] args){
+    public static void main(String[] args){
 
         int aux[] = {49, 43, 65, 45, 67, 68, 97};
         Hashtable<Integer, Integer> tablahash = new Hashtable<>();
-        /* La palabra reservada es HashTable <Tipo de dato, tipo de llave>
+        /* La palabra reservada es Hashtable <Tipo de dato, tipo de llave>
            seguido por el nombre de la tabla = la creacion de objeto */
 
-        for (int i=0; i<=aux.length;i++){
+        for (int i=0; i<aux.length;i++){
             tablahash.put(Math.abs(aux[i])%7, aux[i]);
         }
 
         System.out.println(tablahash);
+
+        // busqueda en tabla hash "contains" y "get"
+        System.out.println("el valor 97 se encuentra en : "+tablahash.contains(97));
+        System.out.println("cuenta con posicion 6 : "+tablahash.contains(6));
+        System.out.println("valor de la posicion 6 : "+tablahash.get(6));
+
+        // eliminar en tabla hash
+        tablahash.remove(6);
+
+        //verificar si esta vacia
+        System.out.println("la tabla hash esta vacia? "+tablahash.isEmpty());
+
+        // vaciar tabla hash
+        tablahash.clear();
     }
 }
