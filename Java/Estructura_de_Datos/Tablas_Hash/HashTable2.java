dbb11e43a78589369f381a5a19ad12a64dfbb741
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-10-10 08:29:13 -0500

    Create HashTable2.java
    class file lesson Hash table

---
+++ Java/Estructura_de_Datos/Tablas_Hash/HashTable2.java
@@ -0,0 +1,18 @@
+import java.util.HashTable; /* La declaracion de la tabla hash se realiza importando este conjunto */
+
+public class HashTable2 {
+
+    public static void main(String[ ] args){
+
+        int aux = {49, 43, 65, 45, 67, 68, 97};
+        HashTable<Integer, Integer> tablahash = new HashTable<>();
+        /* La palabra reservada es HashTable <Tipo de dato, tipo de llave>
+           seguido por el nombre de la tabla = la creacion de objeto */
+
+        for (int i=0; i<=aux.length;i++){
+            tablahash.put(Math.abs(aux[i])%7, aux[i]);
+        }
+
+        System.out.println(tablahash);
+    }
+}
