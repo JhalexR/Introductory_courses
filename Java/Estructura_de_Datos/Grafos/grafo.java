bbd2e2df6dc1405d40ae64088344329c0858b3e6
John Alexander Peñaloza Rojas <rojas.jhalex@gmail.com>
2024-10-13 12:36:31 -0500

    Create grafo.java
    file lesson on Grafos

---
+++ Java/Estructura_de_Datos/Grafos/grafo.java
@@ -0,0 +1,37 @@
+package Estructura_de_Datos.Grafos;
+
+public class grafo {
+
+    private boolean A[][];
+
+    public grafo (int nodos){
+        A = new boolean [nodos][nodos];
+    }
+
+    public void agregarArco(int i, int j){
+        A[i][j]=true;
+        A[j][i]=true;
+    }
+
+    public void eliminarArco(int i, int j){
+        A[i][j]=false;
+        A[j][i]=false;
+    }
+
+    public boolean existeArco(int i, int j){
+        return A [i][j];
+    }
+
+    public static void main (String[ args]) {
+        Grafo grafo = new Grafo(5);
+        grafo.agregarArco(1,2);
+        grafo.agregarArco(1,3);
+
+        System.out.println(grafo.existeArco(1,2));
+        System.out.println(grafo.existeArco(3,1));
+        System.out.println(grafo.existeArco(1,4));
+        grafo.eliminarArco(1,3);
+        System.out.println(grafo.existeArco(1,3));
+    }
+
+}
