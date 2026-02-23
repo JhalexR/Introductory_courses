0add8ca72adf349c35245f755b8daae8f39c9ee5
Jhalex <rojas.jhalex@gmail.com>
2026-02-23 09:34:57 -0500

    Update Operadores.js
    Update and explanation about operators

--- Javascript/Nuevo curso/Javascript/Operadores.js
+++ Javascript/Nuevo curso/Javascript/Operadores.js
@@ -36,4 +36,44 @@ console.log(a <= b); // true
 // operadores logicos
 console.log(a > 0 && b > 0); // true
 console.log(a > 0 || b < 0);    // true
-console.log(!(a > 0));           // false   
+console.log(!(a > 0));           // false  
+
+// operadores de incremento y decremento
+a++; // a = a + 1; => a = 1
+a--; // a = a - 1; => a = 0 
+++a; // a = a + 1; => a = 1
+--a; // a = a - 1; => a = 0
+
+// operadores de igualdad estricta
+console.log(a === b); // false
+console.log(a !== b); // true
+console.log(3 === '3'); // false (tipos diferentes)
+
+// operadores de desigualdad estricta
+console.log(3 !== '3'); // true (tipos diferentes)
+
+// operadores de igualdad
+console.log(a == b); // false
+console.log(a != b); // true
+
+// operadores de tipo
+console.log(typeof a); // number
+console.log(typeof b); // number
+
+// operadores de concatenacion
+let str1 = "Hola";
+let str2 = "Mundo";
+console.log(str1 + " " + str2); // Hola Mundo
+
+// operadores de exponenciacion
+a = 2;
+console.log(a ** 3); // 8 (2 elevado a la potencia de 3)
+
+// operadores de division entera
+console.log(Math.floor(a / 2)); // 1 (división entera de a entre 2)
+
+// operadores de resto
+console.log(a % 2); // 0 (resto de a dividido entre 2)
+
+// operadores de asignacion de exponenciacion
+a **= 3; // a = a ** 3; => a = 8
