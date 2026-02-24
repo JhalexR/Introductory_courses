f9d73c11502d4820bb3aa4bc418c1e91cf20f4c0
Jhalex <rojas.jhalex@gmail.com>
2026-02-24 13:49:23 -0500

    Create 05_Ejercicios_Operadores.js
    small file of exercises with operators

---
+++ Javascript/Nuevo curso/Javascript/05_Ejercicios_Operadores.js
@@ -0,0 +1,16 @@
+// Ejercicios - Operadores 
+
+//calcule el número de segundos que existen en un día.
+let medida = 60*60*24;
+console.log(medida);
+
+//calcule el área de un rectángulo.
+let ancho = 3;
+let alto = 5;
+let area = ancho * alto;
+console.log(area);
+
+// Hacer un conversor de grados centígrados a grados Fahrenheit.
+let centrigrados = 20;
+let fahrenheit = centrigrados * 9/5 + 32;
+console.log(fahrenheit);
