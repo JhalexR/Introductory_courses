469226064cc3595c7b152302d2cfe4e090d0d2c6
Jhalex <rojas.jhalex@gmail.com>
2026-02-25 19:16:06 -0500

    Create 10_Funciones.js
    initial explanation of functions

---
+++ Javascript/Nuevo curso/Javascript/10_Funciones.js
@@ -0,0 +1,26 @@
+//funciones
+
+// las funciones pueden ser tratadas como un valor 
+
+// funciones de forma declarativa
+function saludar (nombre){
+	console.log('hola soy ${nombre}');
+}
+
+// usar función;
+saludar('Alex');
+
+function saludar (nombre){
+	return 'hola soy ${nombre}'
+}
+
+var saludo = saludar('Alex');
+
+comsole.log('saludo');
+
+comsole.log(saludar('Alex'));
+
+// funciones de expresión o anónimas
+var suma = function (a, b){ //no se le asigna un nombre
+	return a + b;
+}
